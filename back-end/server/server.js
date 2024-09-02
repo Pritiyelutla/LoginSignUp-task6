7dc085fb5c5d7078a8e5a38d3daed31ecd1aa52a
Priti Yelutla <pritiyelutla@gmail.com>
2024-09-02 23:13:46 +0530

    init project

---
+++ back-end/server/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const authRoutes = require('./routes/auth');
+
+dotenv.config();
+
+const app = express();
+
+// Middleware // DONE msg dikhao
+app.use(express.json());
+app.use(cors());
+
+// Database connection
+mongoose.connect("mongodb://localhost:27017/my-task-6", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => console.log('MongoDB connected'))
+  .catch((err) => console.log(err));
+
+// Routes
+app.use('/api/auth', authRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
